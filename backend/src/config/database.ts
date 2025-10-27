92931731bfd674a091f8fd0705f1b38fcda45645
Gustavo Crisosth <favocrisosth5@gmail.com>
2025-10-27 10:37:22 -0300

    feat: Adiciona scripts de build e start para produção

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,13 +1,25 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Dialect } from 'sequelize';
 
 const dbName = process.env.DB_NAME || 'portfolio_db';
 const dbUser = process.env.DB_USER || 'gustavo';
 const dbPass = process.env.DB_PASS || 'gustavopass';
 const dbHost = process.env.DB_HOST || 'localhost';
+const dbDialect = 'mysql' as Dialect;
+
+const useSsl = (dbHost !== 'localhost' && dbHost !== 'db');
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
     host: dbHost,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+
+        ...(useSsl ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        } : {})
+    }
 });
 
 export const testConnection = async () => {
